dbd391ad8e568abe86a9bea3bc7fa2d163f1f89b
NiazMorshed2007 <niazmorshed2007@gmail.com>
2022-05-11 22:27:51 +0600

    ....

--- src/pages/create/CreateHeader.jsx
+++ src/pages/create/CreateHeader.jsx
@@ -8,7 +8,7 @@ const CreateHeader = () => {
     navigate(-1);
   };
   return (
-    <header className="flex p-7 border-b border-gray-200 items-center justify-end">
+    <header className="flex fixed w-full p-7 border-b border-gray-200 items-center justify-end">
       <i onClick={goBack} className="text-2xl cursor-pointer">
         <MdOutlineClear />
       </i>
--- src/pages/create/CreateOrganization.jsx
+++ src/pages/create/CreateOrganization.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import { Button } from "antd";
+import { doc, setDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { SetGlobalLoading } from "../../actions";
+import { db } from "../../firebase/firebase";
+import { generateId } from "../../functions/idGenerator";
 
 const CreateOrganization = () => {
-  return <div>CreateOrganization</div>;
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isGlobalLoading = useSelector((state) => {
+    return state.isGlobalLoading;
+  });
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name !== "") {
+      dispatch(SetGlobalLoading(true));
+      setDoc(
+        doc(db, "organizations", generateId(name), {
+          org_name: name,
+          org_id: generateId(name),
+          projects: [],
+        })
+      );
+    }
+  };
+  return (
+    <div className=" flex h-screen justify-center flex-col px-32 w-full">
+      <h1 className="text-4xl">Create Organization</h1>
+      <form onSubmit={handleSubmit} className="mt-16 w-full">
+        <div className="label-inp w-full">
+          <input
+            className="w-full"
+            value={name}
+            onChange={handleChange}
+            required
+            id="name"
+            name="name"
+            type="text"
+          />
+          <label htmlFor="email">Organization Name</label>
+        </div>
+        <div className="button-wrapper w-full justify-end flex gap-5">
+          <Button htmlType="submit" className=" primary-btn-unfilled">
+            Create
+          </Button>
+          <Button
+            onClick={() => {
+              navigate(-1);
+            }}
+            htmlType="cancel"
+            className="default-btn-unfilled"
+          >
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreateOrganization;
